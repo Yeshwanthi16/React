b757cef8de81a6aa5d22b25c9c7a5f52123bff08
yeshwanth-dks <yeshwanth.idiga@nextuple.com>
2023-04-13 15:33:29 +0530

    testing

--- src/Components/Expenses/ExpenseDate.js
+++ src/Components/Expenses/ExpenseDate.js
@@ -4,7 +4,7 @@ const ExpenseDate = (props) =>
 {
     const month = props.date.toLocaleString('en-US', { month: 'long' });
     const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-    // const year = props.date.getFullYear();
+     const year = props.date.getFullYear();
 
     return (
         <div className="expense-date">
