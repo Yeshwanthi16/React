31cc47e4aa32f8296f9043a055d8f8d38ffa2629
parmeet-dks <parmeet.singh@dcsg.com>
2023-04-13 15:00:25 +0530

    Working with Multiple States

--- src/Components/NewExpenses/ExpenseForm.js
+++ src/Components/NewExpenses/ExpenseForm.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 import './ExpensesForm.css';
 
 const ExpenseForm = () => {
 
+const [enteredTitle, setEnteredTitle] = useState('');
+const [enteredAmount, setEnteredAmount] = useState('');
+const [enteredDate, setEnteredDate] = useState('');
+
 const textevent = (event) =>
 {
-    console.log(event.target.value);
-};
+    setEnteredTitle(event.target.value)
+}
+
+const amountEvent = (event) =>
+{
+    setEnteredAmount(event.target.value)
+}
+
+const dateEvent = (event) =>
+{
+    setEnteredDate(event.target.value)
+}
     return (
         <form>
             <div className="new-expense__controls">
@@ -19,13 +33,13 @@ const textevent = (event) =>
             <div className="new-expense__controls">
                 <div className=".new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min='0.01' step='0.01' />
+                    <input type="number" min='0.01' step='0.01' onChange={amountEvent}/>
                 </div>
             </div>
             <div className="new-expense__controls">
                 <div className=".new-expense__control">
                     <label>Date</label>
-                    <input type="date"  min='2023-01=01' max='2100-01-01'/>
+                    <input type="date"  min='2023-01=01' max='2100-01-01' onChange={dateEvent}/>
                 </div>
             </div>
             <div className="new-expense__actions">
